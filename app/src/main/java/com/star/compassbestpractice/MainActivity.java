a12c06f805f2f7eeba33ea2981ff5d25dadc46be
crystalDf <chendong333@gmail.com>
2015-07-24 07:46:03 +0800

    Chapter 12 CompassBestPractice

---
+++ app/src/main/java/com/star/compassbestpractice/MainActivity.java
@@ -0,0 +1,76 @@
+package com.star.compassbestpractice;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener2;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+
+    private SensorEventListener2 mSensorEventListener2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Sensor magneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorEventListener2 = new SensorEventListener2() {
+            @Override
+            public void onFlushCompleted(Sensor sensor) {
+
+            }
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float[] magneticValues = new float[3];
+                float[] accelerometerValues = new float[3];
+
+                if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                    magneticValues = event.values.clone();
+                } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    accelerometerValues = event.values.clone();
+                }
+
+                float[] r = new float[9];
+
+                float[] values = new float[3];
+
+                SensorManager.getRotationMatrix(r, null, accelerometerValues, magneticValues);
+                SensorManager.getOrientation(r, values);
+
+                Log.d("MainActivity", "values[0] is " + Math.toDegrees(values[0]));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        mSensorManager.registerListener(mSensorEventListener2,
+                magneticSensor, SensorManager.SENSOR_DELAY_GAME);
+
+        mSensorManager.registerListener(mSensorEventListener2,
+                accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(mSensorEventListener2);
+        }
+    }
+}
