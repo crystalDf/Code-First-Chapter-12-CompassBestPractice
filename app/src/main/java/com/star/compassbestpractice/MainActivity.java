5f2bf0202548381da77c65fffc3483d1c8a978aa
crystalDf <chendong333@gmail.com>
2015-07-25 21:48:59 +0800

    Chapter 12 CompassBestPractice add Compass

--- app/src/main/java/com/star/compassbestpractice/MainActivity.java
+++ app/src/main/java/com/star/compassbestpractice/MainActivity.java
@@ -6,7 +6,9 @@ import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
     private SensorEventListener2 mSensorEventListener2;
 
+    private ImageView mCompassImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+        mCompassImageView = (ImageView) findViewById(R.id.compass_image);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -26,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         Sensor accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         mSensorEventListener2 = new SensorEventListener2() {
+
+            float[] magneticValues = new float[3];
+            float[] accelerometerValues = new float[3];
+
+            float lastRotateDegree;
+
             @Override
             public void onFlushCompleted(Sensor sensor) {
 
@@ -33,9 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSensorChanged(SensorEvent event) {
-                float[] magneticValues = new float[3];
-                float[] accelerometerValues = new float[3];
-
                 if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
                     magneticValues = event.values.clone();
                 } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -49,7 +61,22 @@ public class MainActivity extends AppCompatActivity {
                 SensorManager.getRotationMatrix(r, null, accelerometerValues, magneticValues);
                 SensorManager.getOrientation(r, values);
 
-                Log.d("MainActivity", "values[0] is " + Math.toDegrees(values[0]));
+//                Log.d("MainActivity", "values[0] is " + Math.toDegrees(values[0]));
+
+                float rotateDegree = (float) - Math.toDegrees(values[0]);
+
+                if (Math.abs(rotateDegree - lastRotateDegree) > 1) {
+                    RotateAnimation rotateAnimation = new RotateAnimation(
+                            lastRotateDegree, rotateDegree, Animation.RELATIVE_TO_SELF, 0.5f,
+                            Animation.RELATIVE_TO_SELF, 0.5f
+                    );
+
+                    rotateAnimation.setFillAfter(true);
+
+                    mCompassImageView.startAnimation(rotateAnimation);
+
+                    lastRotateDegree = rotateDegree;
+                }
             }
 
             @Override
